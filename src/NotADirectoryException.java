a3fd4ddb32d6a7b43eb3af08bb9aa359af96e1a9
Andrew Ferruzza <45921900+aferruzza@users.noreply.github.com>
2020-05-08 18:26:10 -0400

    Update NotADirectoryException.java

--- src/NotADirectoryException.java
+++ src/NotADirectoryException.java
@@ -1,11 +1,3 @@
-/*
- * Andrew Ferruzza
- * STUDENT ID : 111616974
- * HW 5 : Bash Shell
- * 
- * Exception Class : NOT A DIRECTORY
- * Function - creates NotADirectoryException using inheritance from the parent class Exception
- */
 public class NotADirectoryException extends Exception {
 	public NotADirectoryException() { super(); }
 	public NotADirectoryException(String message) { super(message); }
