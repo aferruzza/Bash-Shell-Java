999b19dcc1bca0cc33a7b17e50185421e20201de
Andrew Ferruzza <45921900+aferruzza@users.noreply.github.com>
2020-05-08 18:25:58 -0400

    Update FullDirectoryException.java

--- src/FullDirectoryException.java
+++ src/FullDirectoryException.java
@@ -1,11 +1,3 @@
-/*
- * Andrew Ferruzza
- * STUDENT ID : 111616974
- * HW 5 : Bash Shell
- * 
- * Exception Class : FULL DIRECTORY
- * Function - creates FullDirectoryException using inheritance from the parent class Exception
- */
 public class FullDirectoryException extends Exception {
 	public FullDirectoryException() { super(); }
 	public FullDirectoryException(String message) { super(message); }
